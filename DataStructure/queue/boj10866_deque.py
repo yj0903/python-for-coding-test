c409b69f48b9ad1a8bce2123a40b3a97ef473a23
yj0903 <wiseidea1@hanmail.net>
2022-07-28 00:00:36 +0900

    boj10866 덱 (실버4)

--- DataStructure/queue/boj10866_deque.py
+++ DataStructure/queue/boj10866_deque.py
@@ -1,10 +1,42 @@
+# boj10866 덱 (실버4)
+
 from collections import deque
+import sys
 
-d = deque()
+n = int(input('숫자 입력'))
+dq = deque()
 
-d.appendleft(10)
-d.appendleft(11)
-d.append(100)
-d.append(101)
+for i in range(n):
+    cmd = sys.stdin.readline().split()
 
-print(d)
+    if cmd[0] == "push_front":
+        dq.appendleft(cmd[1])
+    elif cmd[0] == "push_back":
+        dq.append(cmd[1])
+    elif cmd[0] == "pop_front":
+        if len(dq) != 0:
+            print(dq.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == "pop_back":
+        if len(dq) != 0:
+            print(dq.pop())
+        else:
+            print(-1)
+    elif cmd[0] == "size":
+        print(len(dq))
+    elif cmd[0] == "empty":
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == "front":
+        if len(dq) != 0:
+            print(dq[0])
+        else:
+            print(-1)
+    elif cmd[0] == "back":
+        if len(dq) != 0:
+            print(dq[len(dq) - 1])
+        else:
+            print(-1)
