fc862236738a09dd3ba72ab7d80c341ebe52d522
yj0903 <wiseidea1@hanmail.net>
2022-08-07 15:08:24 +0900

    boj5397 키로거 (스택,그리디) - 스택 2개 사용하여 해결

--- DataStructure/stack/boj5397.py
+++ DataStructure/stack/boj5397.py
@@ -1,26 +1,28 @@
 # boj5397 키로거
 # 실버2
 
+# 메모리 41660KB
+# 시간 1640ms
+
 import sys
 
 n = int(sys.stdin.readline())
 
 for i in range(n):
-    result = list()
-    cursor = 0
-
+    left_stack = list()
+    right_stack = list()
     keys = sys.stdin.readline()
 
     for key in keys:
-        if key == '<':
-            cursor = max(0, cursor - 1)
-        elif key == '>':
-            cursor = min(len(result), cursor + 1)
-        elif key == '-':
-            if len(result) != 0:
-                result.pop()
+        if key == '<': # 왼쪽 커서 이동
+            if left_stack:
+                right_stack.append(left_stack.pop())
+        elif key == '>': # 오른쪽 커서 이동
+            if right_stack:
+                left_stack.append(right_stack.pop())
+        elif key == '-': # 값 지움
+            if left_stack:
+                left_stack.pop()
         elif '0' <= key <= '9' or 'a' <= key <= 'z' or 'A' <= key <= 'Z':
-            result.insert(cursor, key)
-            cursor += 1
-
-    print(''.join(result))
+            left_stack.append(key)
+    print(''.join(left_stack) + ''.join(right_stack[::-1]))
