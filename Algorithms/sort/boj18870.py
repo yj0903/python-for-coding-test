1da5cea552490206800ed15279212446fbf1032a
yj0903 <wiseidea1@hanmail.net>
2022-08-10 17:12:12 +0900

    boj18870 좌표압축-딕셔너리 이용 *시간복잡도 O(1)

--- Algorithms/sort/boj18870.py
+++ Algorithms/sort/boj18870.py
@@ -1,12 +1,13 @@
 # boj18870 좌표압축
 # 난이도 실버2
+
 import sys
 
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 
-result = []
-for y in arr:
-    result.append((len(set(filter(lambda x: x < y, arr)))))
-for i in result:
-    print(i, end=' ')
+# 중복제거 후 정렬하면, 본인의 인덱스 위치가 곧 순서
+arr2 = list(sorted(set(arr)))
+dic = {arr2[i]:i for i in range(len(arr2))}
+
+print(' '.join(map(str, [dic[i] for i in arr]))) # 정수형 리스트의 출력은 이렇게 하는 것...!!!!!
