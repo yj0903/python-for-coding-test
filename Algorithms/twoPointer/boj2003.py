ffe79ac49498586bc4b3dc97cee0116bc701b71c
yj0903 <wiseidea1@hanmail.net>
2022-08-12 11:12:15 +0900

    boj2003 수들의 합2(투포인터) #2 상위랭커코드 참고

--- Algorithms/twoPointer/boj2003.py
+++ Algorithms/twoPointer/boj2003.py
@@ -2,23 +2,21 @@
 # 난이도 실버4
 
 # 메모리 30840 KB
-# 시간 588 ms
+# 시간 76 ms
+
+# chsun0303(13437875)
 
 import sys
 
 n, target = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
-left, right = 0, 1
-count = 0
+left, total, count = 0, 0, 0
 
-while left <= right <= n:
-    total = sum(arr[left:right])
-    if total < target:
-        right += 1
-    elif total > target:
+for i in arr:
+    total += i
+    while total > target:
+        total -= arr[left]
         left += 1
-    elif total == target:
-        count += 1
-        left += 1
+    count += (total == target)
 print(count)
