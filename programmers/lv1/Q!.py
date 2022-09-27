b7328d1a0cc8fa0e1d9659722270c78991a29b14
yj0903 <wiseidea1@hanmail.net>
2022-09-27 18:05:47 +0900

    프로그래머스 lv1 스킬체크 Q1

---
+++ programmers/lv1/Q!.py
@@ -0,0 +1,11 @@
+def solution(seoul):
+    answer = 0
+    for i in range(len(seoul)):
+        if seoul[i] == "Kim":
+            answer = i
+        else:
+            pass
+    return "김서방은 " + str(answer) + "에 있다"
+
+
+print(solution(["Jane", "Kim"]))
