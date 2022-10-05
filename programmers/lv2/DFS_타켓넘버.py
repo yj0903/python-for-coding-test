4c181bf8eb7044c85263c3e9255df4166e9fd376
yj0903 <wiseidea1@hanmail.net>
2022-10-05 22:25:10 +0900

    DFS_타켓넘버 (다른코드 리뷰)

--- programmers/lv2/DFS_타켓넘버.py
+++ programmers/lv2/DFS_타켓넘버.py
@@ -25,3 +25,11 @@ print(solution([4, 1, 2, 1], 4))
 
 
 
+# from itertools import product
+# def solution(numbers, target):
+#     l = [(x, -x) for x in numbers] # 모든 인자를 튜플로 만들어 리스트화 함.
+#     print(l)
+#     s = list(map(sum, product(*l)))
+#     print(s)
+#     return s.count(target)
+# print(solution([4, 1, 2, 1], 4))
